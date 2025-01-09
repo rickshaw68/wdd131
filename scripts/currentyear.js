c179d0ac4820fb9f1a129a9a228dc1885fe1940a
Rick Shaw <rickshaw@myyahoo.com>
2025-01-08 21:05:52 -0700

    Home page initial commit

---
+++ scripts/currentyear.js
@@ -0,0 +1,5 @@
+// Dynamically set the current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically set the last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
